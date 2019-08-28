701b3817e684966d4920e7d60ea2b2588b38f75a
xichengxml <xichengxml@163.com>
2019-08-28 09:40:21 +0800

    volatile 更改命名规则

---
+++ src/main/java/com/xicheng/jvm/chapter12/code/code331/VolatileTest.java
@@ -0,0 +1,38 @@
+package com.xicheng.jvm.chapter12.code.code331;
+
+/**
+ * volatile变量自增测试
+ *
+ * @author xichengxml
+ * @date 2019-08-27 14:53:19
+ */
+public class VolatileTest {
+
+	private static volatile int race = 0;
+
+	private static final int THREAD_CNT = 20;
+
+	private static void increace() {
+		race++;
+	}
+
+	public static void main(String[] args) {
+		for (int i = 0; i < THREAD_CNT; i++) {
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					for (int j = 0; j < 1000; j++) {
+						increace();
+					}
+				}
+			}).start();
+		}
+
+		// 等待所有线程执行完成
+		while (Thread.activeCount() > 2) {
+			Thread.yield();
+		}
+
+		System.out.println(race);
+	}
+}
---
+++ src/main/java/com/xicheng/jvm/chapter12/code/code370/SingletonTest.java
@@ -0,0 +1,30 @@
+package com.xicheng.jvm.chapter12.code.code370;
+
+/**
+ * description
+ *
+ * @author liubin52
+ * @date 2019-08-28 09:16:44
+ */
+public class SingletonTest {
+
+	private static /*volatile*/ SingletonTest instanse;
+
+	private SingletonTest() {
+	}
+
+	public static SingletonTest getInstance() {
+		if (instanse == null) {
+			synchronized (SingletonTest.class) {
+				if (instanse == null) {
+					instanse = new SingletonTest();
+				}
+			}
+		}
+		return instanse;
+	}
+
+	public static void main(String[] args) {
+		SingletonTest.getInstance();
+	}
+}
